8f49b3daa28e067ef3393107c6213f5b04e354e2
kalpesh1909 <parmarkaran9876@gmail.com>
2024-10-08 22:23:10 +0530

    first commit

---
+++ frontend/src/Components/Offers/Offers.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Offers.css";
+import exclusive_image from "../Assets/exclusive_image.png";
+
+const Offers = () => {
+  return (
+    <div className="offers">
+      <div className="offers-left">
+      <h1>Premium</h1>
+<h1>Deals Just for You</h1>
+<p>AVAILABLE EXCLUSIVELY ON TOP PRODUCTS</p>
+        <button>Check now</button>
+      </div>
+      <div className="offers-right">
+        <img src={exclusive_image} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Offers;
